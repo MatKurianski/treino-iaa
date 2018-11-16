b6635fd68b45b30b12900cd5f1944f2e580094a2
Mona Lisa <38268201+MatKurianski@users.noreply.github.com>
2018-11-16 19:43:36 -0200

    initial commit

---
+++ lista/questao_1/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+  public static void main(String args[]) {
+    Combinatoria c = new Combinatoria("MAK");
+    c.arranjarEImprimir(3);
+  } 
+}
+
+interface ICombinatoria { // Métodos que um usuário pode chamar.
+  void arranjarEImprimir(int contador);
+}
+
+class Combinatoria implements ICombinatoria {
+  Combinatoria(char[] palavra) {
+    this.palavra = palavra;
+  }
+
+  Combinatoria(String palavra) { // Sobrecarga opcional.
+    this.palavra = palavra.toCharArray();
+  }
+
+  private char[] palavra;
+
+  public void arranjarEImprimir(int contador) {
+    arranjarEImprimirAux(contador, "");
+  } 
+
+  private void arranjarEImprimirAux(int contador, String aux) {
+    if(contador == 0) System.out.println(aux);
+    else
+      for(char letra : this.palavra)
+        arranjarEImprimirAux(contador-1, aux+letra);
+  } 
+}
