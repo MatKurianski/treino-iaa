d45d5bda6ff7e64e6a9c5fbc2592ab9193cf80a1
Mona Lisa <38268201+MatKurianski@users.noreply.github.com>
2018-11-18 20:27:27 -0200

    algoritmo guloso implementado

--- lista/questao_5/Main.java
+++ lista/questao_5/Main.java
@@ -1,70 +1,30 @@
+/* 
+  NOTA: Implementação dos algoritmos pedidos está em GPS e GPSGuloso.
+*/
+
 public class Main {
   public static void main(String args[]) {
     Mapa m = new Mapa(6);
     
-    Ponto partida = new Partida(4, 3);
-    Ponto chegada = new Chegada(5, 1);
+    Ponto partida = new Partida(1, 1);
+    Ponto chegada = new Chegada(6, 6);
 
     m.adicionarPontos(
       partida, 
       chegada,
       new Barreira(3, 3),
       new Barreira(1, 3),
-      new Barreira(6, 6),
       new Barreira(1, 3),
       new Barreira(4, 2),
       new Barreira(2, 5),
       new Barreira(4, 5),
-      new Barreira(5, 4)
+      new Barreira(5, 4),
+      new Barreira(5, 5),
+      new Barreira(6, 5)
     );
-    encontrarCaminho(m, partida);
-    m.imprimirMapa();
-  }
-
-  /*
-    NOTA: É a questão 5, logo, sem otimização nenhuma.
-  */
-
-  public static void encontrarCaminho(Mapa objetoMapa, Ponto partida) {
-    int linha = partida.getLinha();
-    int coluna = partida.getColuna();
-    visita(objetoMapa.Mapa, linha, coluna, "");
-  }
-  
-  public static boolean visita(char[][] mapa, int lin, int col, String ultimoMovimento) {
-    char posicao;
     
-    try {
-      posicao = mapa[lin][col];
-      if(posicao == 'C') {
-        System.out.println("Sucesso!\n");
-        return true;
-      }
-
-      if(posicao == 'X' || posicao == '#') return false;
-      if(posicao != 'P') mapa[lin][col] = '#';
-
-      // Abaixo, o movimento está programado para nunca retroceder um passo
-      // pois, caso necessário, o próprio Backtracking da recursão retrocede.
-
-      if(!ultimoMovimento.equals("baixo")) {
-        if(visita(mapa, lin-1, col, "cima")) return true;
-      }
-      if(!ultimoMovimento.equals("direita")) {
-        if(visita(mapa, lin, col-1, "esquerda")) return true;
-      }
-      if(!ultimoMovimento.equals("esquerda")) {
-        if(visita(mapa, lin, col+1, "direita")) return true;
-      }
-      if(!ultimoMovimento.equals("cima")) {
-        if(visita(mapa, lin+1, col, "baixo")) return true;
-      }
-
-      if(posicao != 'P') mapa[lin][col] = '.';
-      
-      return false;
-    } catch(ArrayIndexOutOfBoundsException e) {
-      return false;
-    }
+    GPSGuloso.encontrarCaminho(m, partida, chegada);
+    
+    m.imprimirMapa();
   }
 }
